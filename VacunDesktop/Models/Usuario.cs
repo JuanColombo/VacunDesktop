fd612027229376722a00df604a9c22cb8424d196
Juan Pablo Colombo <54561024+JuanColombo@users.noreply.github.com>
2021-09-30 13:43:05 -0300

    PeqCambios

--- VacunDesktop/Models/Usuario.cs
+++ VacunDesktop/Models/Usuario.cs
@@ -10,8 +10,11 @@ namespace VacunDesktop.Models
         public int Id { get; set; }
         [Required]
         public string Nombre { get; set; }
+        [Required]
         public string User { get; set; }
+        [Required]
         public string Password { get; set; }
+        [Required]
         public TipoUsuarioEnum TipoUsuario { get; set; }
         public override string  ToString() { return (Nombre); }
     }
